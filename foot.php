7fdbb2b3ab17f2fd96d7e526f0fe4fd7a7e1140e
Subrato Basak <122168347+SubratoBcodez@users.noreply.github.com>
2023-03-19 21:53:21 +0600

    Update foot.php

--- foot.php
+++ foot.php
@@ -1,6 +1,6 @@
 
 <section class="foot">
-        <center><p>2022 &copy; Developed with &hearts; By <a href="/">SubratoB</a> | <a href="/">Belal</a></p></center>
+        <center><p>2022 &copy; Developed with &hearts; By <a href="https://www.facebook.com/basak.boy/">SubratoB</a> | <a href="/">Belal</a></p></center>
 </section>
         <script src="js/jquery.min.js"></script>
         <script src="js/owl.carousel.min.js"></script>
